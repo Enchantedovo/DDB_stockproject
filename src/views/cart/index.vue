da4bd3cd8d3c2c4391939cdd2de834efefd99a91
胡蝶 <2273275376@qq.com>
2020-12-14 13:47:31 +0800

    登录+架构

---
+++ src/views/cart/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    购物车
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    },
+    onCancel() {
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
